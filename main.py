e0ca0316f3f1d08f232e7a8df033a91a1de02cd4
Jonathan Hsu <jhsu0903@hotmail.com>
2022-09-23 17:29:48 -0400

    Initial Move setup

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import os
 
 player_symbol = "X"
+time_limit = 10
+moves_taken = []
 
 def take_turn():
     if os.path.exists("move_file"):
@@ -13,10 +15,15 @@ def take_turn():
 def minimax():
     return
 
+def valid_moves():
+    return
+
 
 if __name__ == '__main__':
     while not os.path.exists("end_game"):
+        if(len(moves_taken) == 0):
+            with open("first_four_moves", "r") as f:
+                moves_taken = f.readlines()
         if os.path.exists("NaturallyUnintelligent.go"):
-            f = open("NaturallyUnintelligent.go")
             take_turn()
     exit()
