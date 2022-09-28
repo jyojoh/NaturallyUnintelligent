1400f3b97d1dcac52d20e5f54a13484239f0c1fd
JCampanale <jcampanale@wpi.edu>
2022-09-28 18:11:42 -0400

    working on minimax implementation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 player_symbol = "O"
+opponent_symbol = "X"
 time_limit = 10
 turn_num = 0
 moves_taken = []
@@ -16,8 +17,10 @@ def take_turn():
             opponent_move = f.readline()
         if os.path.getsize("move_file") == 0 and turn_num == 0:
             player_symbol = "X"
+            opponent_symbol = "O"
     next_board = parse_move(opponent_move) % 9
     next_moves = valid_moves(next_board)
+    minimax(next_moves, False)
 
 
 def coord_convert(i, j):
@@ -34,45 +37,53 @@ def parse_move(str):
 
 def update_board(symbol, index):
     board_state[index] = symbol
-    check_win(index // 9)
+    check_win(index // 9, False)
 
-def check_win(sub_board):
+def check_win(sub_board, eval):
     if boards_won[sub_board] != "None":
         return
 
     center_index = sub_board * 9 + 4
 
     if board_state[center_index -1] == board_state[center_index] and board_state[center_index +1] == board_state[center_index] and board_state[center_index - 1] != "EMPTY": #Center row
-        boards_won[sub_board] = board_state[center_index -1]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index -1]
+            fill_subboard(sub_board)
         return True
     elif board_state[center_index +2] == board_state[center_index+3] and board_state[center_index +4] == board_state[center_index+3] and board_state[center_index +2] != "EMPTY": #Bottom row
-        boards_won[sub_board] = board_state[center_index +2]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index +2]
+            fill_subboard(sub_board)
         return True
     elif board_state[center_index -4] == board_state[center_index-3] and board_state[center_index -2] == board_state[center_index-3] and board_state[center_index - 4] != "EMPTY": #Top row
-        boards_won[sub_board] = board_state[center_index -4]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index -4]
+            fill_subboard(sub_board)
         return True
     elif board_state[center_index -4] == board_state[center_index-1] and board_state[center_index -4] == board_state[center_index+2] and board_state[center_index - 4] != "EMPTY": #First column
-        boards_won[sub_board] = board_state[center_index - 4]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index - 4]
+            fill_subboard(sub_board)
         return True
     elif board_state[center_index -3] == board_state[center_index-1] and board_state[center_index -3] == board_state[center_index+3] and board_state[center_index - 3] != "EMPTY": #Second column
-        boards_won[sub_board] = board_state[center_index -3]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index -3]
+            fill_subboard(sub_board)
         return True
     elif board_state[center_index -2] == board_state[center_index+1] and board_state[center_index -2] == board_state[center_index+4] and board_state[center_index - 2] != "EMPTY": #Third column
-        boards_won[sub_board] = board_state[center_index -2]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index -2]
+            fill_subboard(sub_board)
         return True
     elif board_state[center_index -4] == board_state[center_index] and board_state[center_index] == board_state[center_index+4] and board_state[center_index] != "EMPTY": #Top Left Bottom Right Diag
-        boards_won[sub_board] = board_state[center_index -4]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index -4]
+            fill_subboard(sub_board)
         return True
     elif board_state[center_index -2] == board_state[center_index] and board_state[center_index] == board_state[center_index+2] and board_state[center_index] != "EMPTY": #Top Right Bottom Left Diag
-        boards_won[sub_board] = board_state[center_index -2]
-        fill_subboard(sub_board)
+        if not eval:
+            boards_won[sub_board] = board_state[center_index -2]
+            fill_subboard(sub_board)
         return True
 
     return False
@@ -81,11 +92,30 @@ def fill_subboard(sub_board):
     for tile in board_state[sub_board * 9 : sub_board * 9 + 9]:
         tile = boards_won[sub_board]
 
-def min(moveset):
-    max(moveset)
 
-def max(moveset):
-    return
+def minimax(moveset, isMax):
+    scoreArr = []
+    currSymbol
+    if isMax:
+        currSymbol = player_symbol
+    else: 
+        currSymbol = opponent_symbol
+    for idx, move in enumerate(moveset):
+        board_state[move] = currSymbol
+        if check_win(move // 9, True):
+            if isMax:
+                scoreArr.append(1)
+            else:
+                scoreArr.append(-1)
+        else:
+            scoreArr.append(0)
+        min(valid_moves(move // 9), not isMax)
+        board_state[move] = "EMPTY"
+
+    if isMax:
+        return max(scoreArr)
+    else:
+        return min(scoreArr)
 
 def pruning():
     return
